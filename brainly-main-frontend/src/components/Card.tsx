2ba72136c27c9c881a8a08d45772a06bab8f3bbe
syedanasquadri <syedanasquadri123@gmail.com>
2026-02-27 15:45:39 +0530

    updated card UI

--- brainly-main-frontend/src/components/Card.tsx
+++ brainly-main-frontend/src/components/Card.tsx
@@ -11,9 +11,6 @@ export function Card({title, link, type}: CardProps){
      <div className="p-4 bg-white  rounded-md border-gray-250 max-w-72 border min-h-48 min-w-72">
         <div className="flex justify-between">
             <div className="flex items-center text-md">
-                <div className="pr-2 text-gray-500">
-                <ShareIcon/>
-                </div>
                 {title}
             </div>
             <div className="flex items-center">
@@ -21,9 +18,6 @@ export function Card({title, link, type}: CardProps){
                     <a href={link} target="_blank">
                         <ShareIcon />
                     </a>
-                    </div>
-                    <div className="text-gray-500">
-                    <ShareIcon />
                     </div>
             </div>
         </div>
