7de15a2bcdf19d2b1e9d6de5ea4b028a405743f2
syedanasquadri <syedanasquadri123@gmail.com>
2026-02-27 06:37:05 +0530

    updated app.tsx for homepage

--- brainly-main-frontend/src/App.tsx
+++ brainly-main-frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App(){
  return <BrowserRouter>
   <Routes>
+    <Route path="" element={<></>}/>
     <Route path="/signup" element={<Signup/>}/>
     <Route path="/signin" element={<Signin/>}/>
     <Route path="/dashboard" element={<Dashboard/>}/>
